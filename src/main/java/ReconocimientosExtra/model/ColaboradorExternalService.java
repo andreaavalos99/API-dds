a3079fb1e22900697b3c055dc0d72cb4e35b3792
andreaavalos99 <andreaavalos99@gmal.com>
2024-09-10 03:35:53 -0300

    deberia ver como conectar a la base del proyecto

---
+++ src/main/java/ReconocimientosExtra/model/ColaboradorExternalService.java
@@ -0,0 +1,17 @@
+package ReconocimientosExtra.model;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ColaboradorExternalService {
+
+    // Simulación de conexión a una base de datos externa.
+    // public List<Colaborador> obtenerColaboradoresExternos() { }
+
+    // deberia adaptar a los colaboradores de la base de datos principal a la mia con la info necesaria y traerlos a todos
+
+}
